ff846f597bdadeb03275c6fe9cea48a11a751ced
PHILIP <philophilo037@gmail.com>
2022-08-26 21:02:13 +0300

    Completetd assignment

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-for (let age = 30; age < 40; age++) {
-    console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+function writeCards(array, event) {
+    const thankYouCards = [];
+    for (let i = 0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
 }
 
-const gifts = ["teddy bear", "drone", "dolls"];
-
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
-
-    return gifts;
-}
-
-wrapGifts(gifts);
-function writeCards(names, event) {
-    var messages = [];
-
-    for (let i = 0; i < names.length; i++) {
-        var m = "Thank you, " + names[i] + ", for the wonderful " + event + " gift!";
-        messages.push(m);
-        debugger;
-    }
-
-    return messages;
-};
-
-debugger;
-
-const names = ["Ada", "Brendan", "Ali"];
-const event = "birthday";
-console.log(writeCards(names, event));
 function countDown(integer) {
-    var i = integer;
-    while (i >= 0) {
-        console.log(i)
-        i--
-    };
-};
-
-countDown(10);
+    while (integer >= 0) {
+        console.log(integer);
+        integer --;
+    }
+}
